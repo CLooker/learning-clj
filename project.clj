217b4a5ccf9583ad1a6125da9499e401f70dc8f0
Chad Looker <chad.looker@gmail.com>
2020-04-11 15:39:45 -0400

    Update  project

--- project.clj
+++ project.clj
@@ -4,7 +4,10 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [org.clojure/core.async "1.1.587"]]
+                 [org.clojure/core.async "1.1.587"]
+                 [org.clojure/java.jdbc "0.7.11"]
+                 [org.postgresql/postgresql "42.2.12.jre7"]
+                 [clojure.jdbc/clojure.jdbc-c3p0 "0.3.3"]]
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}}
   :source-paths ["src/clj/learning_coreasync.clj"]
